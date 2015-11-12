e148c39636f2044167d05b5f9a80df454fe4c334
Ralf Eggert <ralf@travello.com>
2015-11-12 14:16:23 +0100

    Added example 03_03

--- public/examples/listing_03_02.php
+++ public/examples/listing_03_02.php
@@ -17,15 +17,20 @@ define('APPLICATION_ROOT', realpath(__DIR__ . '/../..'));
 // setup autoloading from composer
 require_once APPLICATION_ROOT . '/vendor/autoload.php';
 
+// create ViewModel and set variables and template
 $viewModel = new ViewModel(['headline' => 'Happy welcome!']);
 $viewModel->setVariable('name', 'Luigi');
 $viewModel->setVariables(['list' => ['Foo', 'Bar', 'Baz']]);
 $viewModel->setTemplate('listing_03_01');
 
+// prepare resolve with path stack
 $resolver = new TemplatePathStack(
     ['script_paths' => [APPLICATION_ROOT . '/templates']]
 );
+
+// prepare renderer
 $renderer = new PhpRenderer();
 $renderer->setResolver($resolver);
 
+// render and output template
 echo $renderer->render($viewModel);
