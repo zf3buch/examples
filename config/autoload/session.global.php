7f2a55ee270b969cc2c0e666cca8f521e62ecc30
Ralf Eggert <ralf@travello.com>
2015-12-22 18:12:38 +0100

    Examples for sessions

--- config/autoload/session.global.php
+++ config/autoload/session.global.php
@@ -1,16 +1,16 @@
 <?php
-return array(
-    'dependencies' => array(
-        'factories' => array(
+return [
+    'dependencies' => [
+        'factories' => [
             Zend\Session\Config\SessionConfig::class =>
                 Zend\Session\Service\SessionConfigFactory::class,
-        ),
-    ),
+        ],
+    ],
 
-    'session_config' => array(
+    'session_config' => [
         'save_path'       => realpath(APPLICATION_ROOT . '/data/session'),
         'name'            => 'MY_SESSION',
         'cookie_lifetime' => 365 * 24 * 60 * 60,
         'gc_maxlifetime'  => 720,
-    ),
-);
+    ],
+];
