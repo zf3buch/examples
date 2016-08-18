571cd70c106e60d04e8b1f9fc27a1be97e012a1f
Ralf Eggert <ralf@travello.com>
2016-08-18 09:18:36 +0200

    Updated layout for examples application; added phpunit listings

---
+++ public/examples/listing_c_05.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ZF3 book examples
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/examples
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+// define application root for better file path definitions
+define('APPLICATION_ROOT', realpath(__DIR__ . '/../..'));
+
+// setup autoloading from composer
+require_once APPLICATION_ROOT . '/vendor/autoload.php';
+
+// load file content
+$fileName    = realpath(
+    APPLICATION_ROOT . '/tests/Basket/PizzaBasketTest.php'
+);
+$fileRows = file($fileName);
+
+for ($count = 22; $count <= 50; $count++) {
+    unset($fileRows[$count]);
+}
+for ($count = 69; $count <= 77; $count++) {
+    unset($fileRows[$count]);
+}
+
+$fileContent = implode('', $fileRows);
+
+echo '<pre>';
+echo htmlspecialchars($fileContent) . "\n";
+echo "</pre>";
